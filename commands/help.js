999a38643d18ec0ce010a6297f1b08971b3cbfb9
Leonardo Moraes <leonardomoraes97@gmail.com>
2021-02-15 15:35:24 -0400

    fixed help.js

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 
-module.exports.help = 'lists all command with a little description',
+module.exports.help = 'Lists all commands with a little description',
 module.exports.run = function (msg, args) {
     const commandListText = fs.readdirSync('./commands')
-        .map(file => `!${file.slice(0,-3)}`)
+        .map(file =>
+            `${"``"}!${file.slice(0,-3)}${"``"} ${require(`./${file}`).help}`)
         .join("\n");
     const helpEmbed = new Discord.MessageEmbed()
         .setColor('#ff0040')
-        .addField('Список команд',`${"```"}${commandListText}${"```"}`)
+        .addField('Список команд',commandListText)
         .setTimestamp();
     msg.channel.send(helpEmbed);
 }
